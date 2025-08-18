f83c8674f70082c817605fe34a221524367f9a6d
Arman7171 <armvar774@gmail.com>
2025-08-18 14:18:36 +0400

    animation creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import EnvelopeAnimation from "./EnvelopeAnimation";
+import FadeSlide from "./FadeSlide";
 
 function App() {
+  const [isOpenFirst, setIsOpenFirst] = useState(true);
+  const [isOpenSecond, setIsOpenSecond] = useState(false);
   const openEnvelope = () => {
-    console.log("Envelope opened!");
+    setTimeout(() => {
+      setIsOpenFirst(false);
+    }, 3000);
+    setTimeout(() => {
+      setIsOpenSecond(true);
+    }, 4000);
   };
   return (
     <>
-      <div className="title">Մենք ամուսնանում ենք</div>
-      <div className="content-between">
-        <div>Հոկտեմբեր 12</div>
-        <div>2025</div>
-      </div>
-      <div className="content-center">
-        <img
-          src="https://optim.tildacdn.one/tild6630-3034-4734-a530-303861363739/-/resize/852x/-/format/webp/Red_and_Beige_Elegan.png.webp"
-          alt=""
-        />
-      </div>
-      <div className="content-center letter-position">
-        <EnvelopeAnimation openEnvelope={openEnvelope} />
-      </div>
+      <FadeSlide
+        show={isOpenFirst}
+        direction="center"
+        distance={120}
+        duration={1}>
+        <div className="content-center">
+          <div>
+            <div className="title">Մենք ամուսնանում ենք</div>
+            <div className="content-between">
+              <div>Հոկտեմբեր 12</div>
+              <div>2025</div>
+            </div>
+          </div>
+        </div>
+        <div className="content-center">
+          <img
+            src="https://optim.tildacdn.one/tild6630-3034-4734-a530-303861363739/-/resize/852x/-/format/webp/Red_and_Beige_Elegan.png.webp"
+            alt=""
+          />
+        </div>
+        <div className="content-center letter-position">
+          <EnvelopeAnimation openEnvelope={openEnvelope} />
+        </div>
+      </FadeSlide>
+      <FadeSlide
+        show={isOpenSecond}
+        direction="center"
+        distance={120}
+        duration={1}>
+        <div className="content-center">
+          <div>
+            <div className="title">Մենք ամուսնանում ենք</div>
+            <div className="content-between">
+              <div>Հոկտեմբեր 12</div>
+              <div>2025</div>
+            </div>
+          </div>
+        </div>
+        <div className="content-center">
+          <img
+            src="https://optim.tildacdn.one/tild6630-3034-4734-a530-303861363739/-/resize/852x/-/format/webp/Red_and_Beige_Elegan.png.webp"
+            alt=""
+          />
+        </div>
+      </FadeSlide>
     </>
   );
 }
--- src/EnvelopeAnimation.tsx
+++ src/EnvelopeAnimation.tsx
@@ -7,20 +7,27 @@ interface PropsType {
 
 const EnvelopeAnimation: React.FC<PropsType> = ({ openEnvelope }) => {
   const [isOpen, setIsOpen] = useState(false);
-
+  const [isScale, setIsScale] = useState(false);
+  const toggleEnvelope = () => {
+    setIsOpen(!isOpen);
+    openEnvelope();
+    setTimeout(() => {
+      setIsScale(true);
+    }, 1000);
+  };
   return (
     <div>
       <div
-        className={`wrapper ${isOpen ? "open" : ""}`}
+        className={`wrapper ${isOpen ? "open" : ""} ${isScale ? "scale" : ""}`}
         onClick={() => {
-          setIsOpen(!isOpen);
+          toggleEnvelope();
           openEnvelope();
         }}>
         <div className="lid one"></div>
         <div className="lid two"></div>
         <div className="envelope"></div>
         <div className="letter">
-          <p>Hello</p>
+          <p className="letter-text">Հրավիրում ենք մեր հարսանիքին</p>
         </div>
       </div>
     </div>

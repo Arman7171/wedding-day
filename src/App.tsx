3b0f7177b1533014c9056bd2ae2f62108971c7bc
Arman7171 <armvar774@gmail.com>
2025-08-18 18:06:34 +0400

    add calendar and music

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import "./App.css";
 import EnvelopeAnimation from "./EnvelopeAnimation";
 import FadeSlide from "./FadeSlide";
+import Calendar from "./Calendar";
 
 function App() {
   const [isOpenFirst, setIsOpenFirst] = useState(true);
   const [isOpenSecond, setIsOpenSecond] = useState(false);
   const openEnvelope = () => {
+    const audio = new Audio("/music.mp3");
+    audio.play();
     setTimeout(() => {
       setIsOpenFirst(false);
     }, 3000);
@@ -15,13 +18,13 @@ function App() {
     }, 4000);
   };
   return (
-    <>
+    <div style={{ maxWidth: "400px", margin: "0 auto" }}>
       <FadeSlide
         show={isOpenFirst}
         direction="center"
         distance={120}
         duration={1}>
-        <div className="content-center">
+        <div className="content-center" style={{ marginTop: "150px" }}>
           <div>
             <div className="title">Մենք ամուսնանում ենք</div>
             <div className="content-between">
@@ -45,23 +48,37 @@ function App() {
         direction="center"
         distance={120}
         duration={1}>
-        <div className="content-center">
-          <div>
-            <div className="title">Մենք ամուսնանում ենք</div>
-            <div className="content-between">
-              <div>Հոկտեմբեր 12</div>
-              <div>2025</div>
+        <div style={{ marginTop: "150px" }}>
+          <div className="content-center">
+            <div>
+              <div className="title">Մենք ամուսնանում ենք</div>
+              <div className="content-between">
+                <div>Հոկտեմբեր 12</div>
+                <div>2025</div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="content-center">
-          <img
-            src="https://optim.tildacdn.one/tild6630-3034-4734-a530-303861363739/-/resize/852x/-/format/webp/Red_and_Beige_Elegan.png.webp"
-            alt=""
-          />
+          <div className="content-center">
+            <img
+              src="https://optim.tildacdn.one/tild6630-3034-4734-a530-303861363739/-/resize/852x/-/format/webp/Red_and_Beige_Elegan.png.webp"
+              alt=""
+            />
+          </div>
+          <div className="subtitle">
+            Սիրով հրավիրում ենք Ձեզ ներկա գտնվելու մեր հարսանյաց արարողությանը։
+          </div>
+          <div>
+            <Calendar />
+          </div>
+          <div className="content-center" style={{ marginTop: "20px" }}>
+            <img
+              src="https://static.tildacdn.one/tild3930-6261-4661-b737-623665393866/Group_738002694.svg"
+              alt=""
+            />
+          </div>
         </div>
       </FadeSlide>
-    </>
+    </div>
   );
 }
 
